4e727d61ec57cd968cf60f89e65a88a2c99dac6c
Sean Park <sean@synim27.local>
2020-07-21 18:10:39 +0900

    33 setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from app            import create_app
 from flask_twisted  import Twisted
 from twisted.python import log
 
-# test2
+# test3
 if __name__ == "__main__":
     app = create_app()
     twisted = Twisted(app)
